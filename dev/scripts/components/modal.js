733a25382afe116bbfe09940c903341853142e3d
Chelsea Keeley <chelsea.keeley@gmail.com>
2017-11-29 10:45:38 -0500

    clean start wed, lets do this

--- dev/scripts/components/modal.js
+++ dev/scripts/components/modal.js
@@ -39,12 +39,12 @@ class TeamModal extends React.Component {
     }
     // User action: submit 'new team' form
 
-    handleSubmit (event){
+    handleSubmit(event) {
         event.preventDefault();
         const dbRef = firebase.database().ref();
         const teamObject = {
             teamName: this.state.teamName,
-            users:{
+            users: {
                 userName: this.state.userName,
                 userEmail: this.state.userEmail,
                 userPhone: this.state.userPhone,
@@ -58,85 +58,85 @@ class TeamModal extends React.Component {
 
     }
 
-        }
-        console.log(teamObject);
-        dbRef.push(teamObject);
-    }
-    // User action: remove focus from form item
-    handleBlur(event){
-        this.setState({
-            [event.target.id] : event.target.value
-        });
-    }
-    openModal() {
-        this.setState({ modalIsOpen: true });
-    }
 
-    }
+// console.log(teamObject);
+// dbRef.push(teamObject);
+    
+// User action: remove focus from form item
+handleBlur(event){
+    this.setState({
+        [event.target.id]: event.target.value
+    });
+}
+openModal() {
+    this.setState({ modalIsOpen: true });
+}
 
-    // Modal controls
-    openModal() {
-        this.setState({ modalIsOpen: true });
-    }
-    afterOpenModal() {
-        // references are now sync'd and can be accessed.
-        this.subtitle.style.color = '#f00';
-    }
+    
 
-    closeModal() {
-        this.setState({ modalIsOpen: false });
-    }
+// Modal controls
+openModal() {
+    this.setState({ modalIsOpen: true });
+}
+afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    this.subtitle.style.color = '#f00';
+}
 
-    render() {
-        return (
-            <div>
-                <button onClick={this.openModal}>+ Add Team</button>
-                <Modal
-                    isOpen={this.state.modalIsOpen}
-                    onAfterOpen={this.afterOpenModal}
-                    onRequestClose={this.closeModal}
-                    style={customStyles}
-                    contentLabel="Example Modal"
+closeModal() {
+    this.setState({ modalIsOpen: false });
+}
 
+render() {
+    return (
+        <div>
+            <button onClick={this.openModal}>+ Add Team</button>
+            <Modal
+                isOpen={this.state.modalIsOpen}
+                onAfterOpen={this.afterOpenModal}
+                onRequestClose={this.closeModal}
+                style={customStyles}
+                contentLabel="Example Modal"
+                >
                     <h2 ref={subtitle => this.subtitle = subtitle}>Team Name</h2>
-                    <button onClick={this.closeModal}>close</button>
-                    {/* <div>I am a modal</div> */}
+        <button onClick={this.closeModal}>close</button>
 
-                    <h2 ref={subtitle => this.subtitle = subtitle}>Team Name</h2>
-                    <button onClick={this.closeModal}>close</button>
+    <h2 ref={subtitle => this.subtitle = subtitle}>Team Name</h2>
+        <button onClick={this.closeModal}>close</button>
 
-                    <form action="" onSubmit={this.handleSubmit}>
-                        <h2>Team Info:</h2>
+        <form action="" onSubmit={this.handleSubmit}>
+            <h2>Team Info:</h2>
 
-                        <label htmlFor="teamName">Team Name:</label>
-                        <input type="text" id="teamName" onBlur={this.handleBlur} />
+            <label htmlFor="teamName">Team Name:</label>
+            <input type="text" id="teamName" onBlur={this.handleBlur} />
 
-                        <h2>Your Info:</h2>
-                        <label htmlFor="userName">Name:</label>
-                        <input type="text" id="userName" onBlur={this.handleBlur} />
+            <h2>Your Info:</h2>
+            <label htmlFor="userName">Name:</label>
+            <input type="text" id="userName" onBlur={this.handleBlur} />
 
-                        <label htmlFor="userEmail">Email:</label>
-                        <input type="text" id="userEmail" onBlur={this.handleBlur} />
+            <label htmlFor="userEmail">Email:</label>
+            <input type="text" id="userEmail" onBlur={this.handleBlur} />
 
-                        <label htmlFor="userPhone">Phone Number:</label>
-                        <input type="text" id="userPhone" onBlur={this.handleBlur} />
+            <label htmlFor="userPhone">Phone Number:</label>
+            <input type="text" id="userPhone" onBlur={this.handleBlur} />
 
-                        <p>Gender:</p>
-                        <label htmlFor="userGenderMale">Male</label>
-                        <input type="radio" id="userGenderMale" name="userGender" onBlur={this.handleBlur} />
-                        <label htmlFor="userGenderFemale">Female</label>
-                        <input type="radio" id="userGenderFemale" name="userGender" onBlur={this.handleBlur} />
+            <p>Gender:</p>
+            <label htmlFor="userGenderMale">Male</label>
+            <input type="radio" id="userGenderMale" name="userGender" onBlur={this.handleBlur} />
+            <label htmlFor="userGenderFemale">Female</label>
+            <input type="radio" id="userGenderFemale" name="userGender" onBlur={this.handleBlur} />
 
-                        <label htmlFor="userPassword">Password:</label>
-                        <input type="password" id="userPassword" onBlur={this.handleBlur} />
-                        <label htmlFor="confirmPassword">Confirm Password:</label>
-                        <input type="password" id="userPassword" onBlur={this.handleBlur} />
-                        <p></p>
-                        <input type="submit" value="Submit" />
-                    </form>
-                </Modal>
-            </div>
+            <label htmlFor="userPassword">Password:</label>
+            <input type="password" id="userPassword" onBlur={this.handleBlur} />
+            <label htmlFor="confirmPassword">Confirm Password:</label>
+            <input type="password" id="userPassword" onBlur={this.handleBlur} />
+            <p></p>
+            <input type="submit" value="Submit" />
+        </form>
+                </Modal >
+            </div >
         );
     }
 }
+
 export default TeamModal;
