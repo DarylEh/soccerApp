783d489173bed0f644fd5cf2477ff7a99c5c9a20
Pat Johnston <pattrashgarbage@gmail.com>
2017-12-05 17:53:00 -0500

    Responsive modal forms

--- dev/scripts/components/addPlayerModal.js
+++ dev/scripts/components/addPlayerModal.js
@@ -147,7 +147,7 @@ class PlayerModal extends React.Component {
                         <label htmlFor="passwordMatch" className="hiddenLabel">Confirm password:</label>
                         <input type="password" id="passwordMatch" name="passwordMatch" onChange={this.handleChange} value={this.state.passwordMatch} placeholder="Confirm Password" required />
 
-                        <input type="submit" value="Submit" />
+                        <input type="submit" value="Add Player" />
                     </form>
                 </Modal>
             </div>
